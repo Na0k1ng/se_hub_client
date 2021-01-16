85232e254734477868d07e01c5778fbd03dba488
n-kawakami <n.kawakami.dev@gmail.com>
2021-01-16 15:31:44 +0900

    情報リスト取得処理変更、POSTに変更

--- src/components/organisms/main/MainContentList.vue
+++ src/components/organisms/main/MainContentList.vue
@@ -68,7 +68,7 @@
                     'count': '1',
                 };
 
-                axios.get('http://localhost:8000/api/disclosure/list/',requestBody ).then(res => {
+                axios.post('http://localhost:8000/api/disclosure/list/',requestBody ).then(res => {
                     if (res.status.toString() === '200') {
                         alert("正常系です。");
                     }
