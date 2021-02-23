d40ee8f0163d0d93f6528ab12c3d4bee188a1f72
n-kawakami <n.kawakami.dev@gmail.com>
2021-02-23 23:18:43 +0900

    vue-fake-inputのインポート方法変更

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
-import VueFakeInput from 'vue-fake-input';
 import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
-Vue.use(VueFakeInput);
 
 export default new Vuetify({
 });
